97a781a86e177cd4c4ba4d2552b2705e3a4b4cc8
Devv Jangidd <devjangid2005bmr@gmail.com>
2026-08-04 17:50:51 +0530

    add request body and handeling json response

--- routes/productRoute.py
+++ routes/productRoute.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, HTTPException , Path
+from fastapi import APIRouter, HTTPException, Path
 from utils.utills import get_all_products
 
 product_routes = APIRouter()
 
-@product_routes.get("/productid/{id}")
-def get_product(id: int):
-    
 
+@product_routes.get("/productid/{id}")
+def get_product(
+    id: int = Path(..., gt=0, description="Product ID")
+):
     products = get_all_products()
-    
 
     for product in products:
         if product["id"] == id:
@@ -18,16 +18,31 @@ def get_product(id: int):
         status_code=404,
         detail="Product not found"
     )
-    
-@product_routes.get("/productname/{name}")
-def get_name(name: str):
-    names = get_all_products()
 
-    for n in names:
-        if n["name"] == name:
-            return n
+
+@product_routes.get("/productname/{name}")
+def get_product_by_path(name: str):
+    products = get_all_products()
+
+    for product in products:
+        if product["name"].lower() == name.lower():
+            return product
 
     raise HTTPException(
         status_code=404,
-        detail="ERROR 404"
+        detail="Product not found"
+    )
+
+
+@product_routes.get("/productname")
+def get_product_by_query(name: str):
+    products = get_all_products()
+
+    for product in products:
+        if product["name"].lower() == name.lower():
+            return product
+
+    raise HTTPException(
+        status_code=404,
+        detail="Product not found"
     )
